bd4670c9ae1577e95e1d14f3e73de3a3a1001331
Alejandro Saez <alejandrosaezc@gmail.com>
2022-04-04 08:44:08 +0200

    Tema4_Ejercicio1

--- Tema4_Ejercicio1.js
+++ Tema4_Ejercicio1.js
@@ -1,2 +1,2 @@
-let cadena = "El perro de mi vecina es muy molesto"
-console.log(cadena.replace("vecina","vecino"))
+let cadena = "El perro de mi vecina es muy molesto";
+console.log(cadena.replace("vecina","vecino"));
